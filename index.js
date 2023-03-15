9029171dfc42dd7e8aa784bafc2e0eaa427d5c60
AndreyNagata <127868432+AndreyNagata@users.noreply.github.com>
2023-03-15 11:28:05 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const menu= document.getElementById("menu") 
+const lista=document.getElementById("lista")
+function amenu(){
+    lista.classList.toggle("active")
+    lista.classList.remove("desactive")
+}
